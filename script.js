27c9a3651cad7228786b4b81f7ed084267c636e2
Vyacheslav <viparamount@gmail.com>
2019-09-05 12:47:29 +0300

    fixed menu appearance

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var $menu = $('.click-menu');
-$('.platform-item__inactive ').click(function(e){
+$('.platform-menu ').click(function(e){
 	$('.platform-menu').toggleClass('.platform-menu__clicked');
 	$menu.toggleClass('hidden')
 	!$menu.hasClass("hidden") ? $menu.appendTo($(e.target).parent()) : $menu.detach();
